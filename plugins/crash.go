fb8b51eac9a427e73c9e437a5cf5a34d02415b14
rjames187 <rory.james2021@gmail.com>
2024-06-23 11:49:41 -0700

    create plugin for deliberately injecting network faults

---
+++ plugins/crash.go
@@ -0,0 +1,53 @@
+package plugins
+
+import (
+	"crypto/rand"
+	"math/big"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Crash struct{}
+
+func fault() {
+	num, _ := rand.Int(rand.Reader, big.NewInt(1000))
+	if num.Int64() < 330 {
+		os.Exit(1)
+	} else if num.Int64() < 660 {
+		bigMS, _ := rand.Int(rand.Reader, big.NewInt(10000))
+		ms := bigMS.Int64() + 5000
+		time.Sleep(time.Duration(ms) * time.Millisecond)
+	}
+}
+
+func (c Crash) Map(input string) []*KeyValue {
+	fault()
+	res := []*KeyValue{}
+	for _, word := range strings.Fields(input) {
+		pair := KeyValue{word, "1"}
+		res = append(res, &pair)
+	}
+	return res
+}
+
+func (c Crash) Reduce(pairs []*KeyValue) []*KeyValue {
+	fault()
+	counter := map[string]*KeyValue{}
+	for _, p := range pairs {
+		count, _ := strconv.Atoi(p.Value)
+		_, present := counter[p.Key]
+		if present {
+			oldCount, _ := strconv.Atoi(counter[p.Key].Value)
+			counter[p.Key].Value = strconv.Itoa(count + oldCount)
+		} else {
+			counter[p.Key] = &KeyValue{Key: p.Key, Value: strconv.Itoa(count)}
+		}
+	}
+	res := []*KeyValue{}
+	for _, kv := range counter {
+		res = append(res, kv)
+	}
+	return res
+}
